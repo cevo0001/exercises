1f71da47f59f41a94eb4619a3866636f119c1da1
Celine Sophie <cevo0001@stud.kea.dk>
2025-09-02 14:04:59 +0200

    øvelse breadcrums opg

--- breadcrums/script.js
+++ breadcrums/script.js
@@ -1,20 +1,24 @@
-const bc = [{ name: "Hvidevarer", link: "/hvidevarer" },{ name: " Vaskemaskiner", link: "/hvidevarer/vaskemaskiner" },{ name: " Bosch", link: "/hvidevarer/vaskemaskiner/bosch/" },];
-
-
+const bc = [{ name: "Hvidevarer", link: "/hvidevarer" } ,{ name: " Vaskemaskiner", link: "/hvidevarer/vaskemaskiner" },{ name: " Bosch", link: "/hvidevarer/vaskemaskiner/bosch/" },];
 
 document.querySelector("button").addEventListener("click", klik);
+// lærer giver en ny funktion, der kan blive brugbar fremad:    // &nbsp; sætter mellemrum ind mellem teksten fast. 
 
 function klik() {
+  let str = "";
 
-    let str = "";
-    // &nbsp; sætter mellemrum ind mellem teksten fast. 
-    console.log(str);
+  bc.forEach((elm, index) => {
+    // tilføj skråstreg kun hvis det IKKE er første element
+    if (index > 0) {
+      str += " / ";
+    }
 
-    bc.forEach(elm =>{
-        // console.log("elm", elm.name);
-        str += `<a href="">${elm.name}</a>`
-    });
+    // hvis det er sidste element -> ingen link
+    if (index === bc.length - 1) {
+      str += `${elm.name}`;
+    } else {
+      str += `<a href="${elm.link}">${elm.name}</a>`;
+    }
+  });
 
-
-    document.querySelector("ul").innerHTML = str;
+  document.querySelector("ul").innerHTML = str;
 }
