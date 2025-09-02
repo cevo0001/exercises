804cb49bacc7a2af74c861229fef062f57406515
Celine Sophie <cevo0001@stud.kea.dk>
2025-09-02 13:18:38 +0200

    ikke færdigt endnu

---
+++ bar_chart_starter_kit2/script.js
@@ -0,0 +1,15 @@
+const list = document.querySelector('ul');
+
+const barArr = [];
+
+setInterval(generateBars, 500);
+
+function generateBars() {
+
+    function randomNum(){
+        const randomNum = Math.floor(Math.random * 100);
+
+    barArr.push();
+    console.log(barArr);
+    }
+}
